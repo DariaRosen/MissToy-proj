a4c448fb8f53169df6d564ff11b450e148b9e8d1
Daria Rosen <daria.sk135@gmail.com>
2025-07-11 19:47:19 +0300

    change some app header style

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -7,23 +7,23 @@ export function AppHeader() {
     const navigate = useNavigate()
 
     return (
-    <header className="app-header">
-        <section className="container">
-            <h1>Toys</h1>
-            <nav>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/toy'>Toys</NavLink>
-            </nav>
-        </section>
-        <section className="container">
-            {user &&
-                <span className="user-info">
-                    {user.imgUrl && <img src={user.imgUrl} />}
-                    {user.fullname}
-                    <span className="score">{user.balance?.toLocaleString()}</span>
-                </span>
-            }
-        </section>
-    </header>
-)
+        <header className="app-header">
+            <section className="container">
+                <h1>Toys</h1>
+                <nav className="main-nav">
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/toy'>Toys</NavLink>
+                </nav>
+            </section>
+            <section className="container">
+                {user &&
+                    <span className="user-info">
+                        {user.imgUrl && <img src={user.imgUrl} />}
+                        {user.fullname}
+                        <span className="score">{user.balance?.toLocaleString()}</span>
+                    </span>
+                }
+            </section>
+        </header>
+    )
 }
